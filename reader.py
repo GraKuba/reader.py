473432409467205f47cef2e809a59576dddbc179
Jakub <grabarczyk.kuba@gmail.com>
2022-04-14 13:52:23 +0200

    Changes to the file from terminal possible; input now from sys.argv[1]

--- reader.py
+++ reader.py
@@ -6,7 +6,11 @@ import pickle
 # GLOBAL VARIABLES
 
 temporary_list = []
+file_in = sys.argv[1]
 action = sys.argv[2]
+location_x = int(sys.argv[3])
+location_y = int(sys.argv[4])
+command_change = sys.argv[5]
 
 # CLASSES
 
@@ -33,23 +37,26 @@ file_pickle = Pickle("file.pickle", pickle, "b")
 
 # DOWNLOAD, READ AND WRITE THE FILE
 
-if action == "json":
+if action == "file.json":
     f_out = file_json.file_out
     f_type = file_json.file_type
     f_arg = file_json.argument
-elif action == "pickle":
+elif action == "file.pickle":
     f_out = file_pickle.file_out
     f_type = file_pickle.file_type
     f_arg = file_pickle.argument
-elif action == "csv":
+elif action == "file.csv":
     ...
 else:
     print("Wrong command!")
 
-with open("file.csv", 'r') as f:
+with open(file_in, 'r') as f:
     reader = csv.reader(f)
     for line in reader:
         temporary_list.append(line)
+
+temporary_list[location_x][location_y] = command_change
+
 if action == "csv":
     with open("file_2.csv", "w") as f:
         writer = csv.writer(f)
